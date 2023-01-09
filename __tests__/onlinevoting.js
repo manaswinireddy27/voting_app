19281eb6c38859e59721f96233f8e22877f59360
manaswinireddy27 <sarasanimanaswini@gmail.com>
2023-01-10 02:50:54 +0530

    updated tests

--- __tests__/onlinevoting.js
+++ __tests__/onlinevoting.js
@@ -13,7 +13,7 @@ function extractCsrfToken(res) {
 }
 
 const login = async (agent, username, password) => {
-    let res = await agent.get("/adminlogin");
+    let res = await agent.get("/login");
     let csrfToken = extractCsrfToken(res);
     res = await agent.post("/session").send({
       email: username,
